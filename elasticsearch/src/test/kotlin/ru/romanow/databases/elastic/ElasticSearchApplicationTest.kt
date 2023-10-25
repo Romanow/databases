e142726ccff345a71100408dc4d7d04b5b1e7067
aromanov <aromanov@inno.tech>
2023-10-25 12:33:20 +0300

    fix Elastic docker image

--- elasticsearch/src/test/kotlin/ru/romanow/databases/elastic/ElasticSearchApplicationTest.kt
+++ elasticsearch/src/test/kotlin/ru/romanow/databases/elastic/ElasticSearchApplicationTest.kt
@@ -11,6 +11,7 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 import org.testcontainers.utility.DockerImageName.parse
+import java.time.Duration.ofSeconds
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -38,7 +39,8 @@ class ElasticSearchApplicationTest {
                 HttpWaitStrategy()
                     .forPath("/_cluster/health")
                     .forStatusCode(200)
-                    .forResponsePredicate { it.contains("\"status\":\"green\"") })
+                    .withStartupTimeout(ofSeconds(120))
+            )
 
         @JvmStatic
         @DynamicPropertySource
