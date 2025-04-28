858f554db889d54c98d321f6dade7d5033e5d83e
romanow <romanowalex@mail.ru>
2025-04-28 21:57:34 +0300

    Update dependencies

--- elasticsearch/src/test/kotlin/ru/romanow/databases/elastic/ElasticSearchApplicationTest.kt
+++ elasticsearch/src/test/kotlin/ru/romanow/databases/elastic/ElasticSearchApplicationTest.kt
@@ -1,15 +1,12 @@
 package ru.romanow.databases.elastic
 
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchConnectionDetails
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
-import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.elasticsearch.ElasticsearchContainer
 
 @ActiveProfiles("test")
@@ -22,7 +19,6 @@ class ElasticSearchApplicationTest {
 
     @TestConfiguration
     internal class ElasticSearchTestConfiguration {
-        private val logger = LoggerFactory.getLogger(ElasticSearchTestConfiguration::class.java)
 
         @Bean
         @ServiceConnection
@@ -30,7 +26,6 @@ class ElasticSearchApplicationTest {
             return ElasticsearchContainer(ELASTIC_IMAGE)
                 .withExposedPorts(EXPOSED_PORT)
                 .withEnv("xpack.security.enabled", "false")
-                .withLogConsumer { Slf4jLogConsumer(logger) }
         }
 
         companion object {
